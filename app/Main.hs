792ab7f32b9a3f41be948d6c082d2f10488293af
sphaso <sphaso@gmail.com>
2019-10-05 12:42:19 +0200

    benchmark edges

--- app/Main.hs
+++ app/Main.hs
@@ -9,8 +9,20 @@ module Main where
   gmedium :: Graph Int
   gmedium = overlay (vertices [1..5]) (clique [6..10])
 
+  gsmallE :: [(Int, Int)]
+  gsmallE = [(x, y) | x<-[1..10], y<-[1..10]]
+
   gbig :: Graph Int
   gbig = overlay (vertices [1..25]) (clique [26..50])
+
+  gmediumE :: [(Int, Int)]
+  gmediumE = [(x, y) | x<-[1..100], y<-[1..100]]
+
+  gbigE :: [(Int, Int)]
+  gbigE = [(x, y) | x<-[1..1000], y<-[1..1000]]
+
+  ghugeE :: [(Int, Int)]
+  ghugeE = [(x, y) | x<-[1..10000], y<-[1..10000]]
 
   group :: String -> Graph Int -> Benchmark
   group label g = bgroup label  [
@@ -19,10 +31,4 @@ module Main where
                    ]
 
   main :: IO ()
-  main = defaultMain [
-                        group "small" gsmall,
-                        group "medium" gmedium,
-                        group "medium clique" (clique [1..10]),
-                        group "big" gbig,
-                        group "big clique" (clique [1..50])
-                     ]
+  main = defaultMain [bench "small" $ nf edges gsmallE, bench "medium" $ nf edges gmediumE, bench "big" $ nf edges gbigE, bench "huge" $ nf edges ghugeE ]
